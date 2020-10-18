4a2f294b1c0651035f4d044dc11f026885b194cb
Bochkarev Ivan <ivanx86@gmail.com>
2020-10-19 00:27:02 +0600

    Обновленная структура сайтов

---
+++ src/site/assets/js/main.js
@@ -0,0 +1,14 @@
+//=require ../../../../node_modules/jquery/dist/jquery.min.js
+
+'use strict';
+
+const Site = {
+  init: () => {
+    Site.Base.init();
+  },
+  Base: {
+    init: () => {},
+  },
+};
+
+$(document).ready(() => Site.init());
